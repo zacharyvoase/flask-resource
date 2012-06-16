feb67d840394dac9fb80115a5857d2050248fbd7
Zachary Voase <z@zacharyvoase.com>
2012-06-16 22:43:09 +0000

    Added setuptools boilerplate, and README conversion via pandoc.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from __future__ import with_statement
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='flask-resource',
+    version='0.0.1',
+    author='Zachary Voase',
+    author_email='z@zacharyvoase.com',
+    url='https://github.com/zacharyvoase/flask-resource',
+    description="Build resource-oriented apps with Flask.",
+    long_description=long_description,
+    packages=find_packages(exclude=('tests',)),
+)
